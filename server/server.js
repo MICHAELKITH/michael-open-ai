b1646c050a3789dc7506ea6ee631fed2bad82294
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-01-28 14:03:22 +0300

    client and server side

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config()
 
